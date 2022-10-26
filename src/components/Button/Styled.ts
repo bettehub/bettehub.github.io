8c41f837555a6f2d9ccff6d8835d726e854c65d5
bettehub <bettehub@gmail.com>
2022-10-27 01:20:18 +0900

    eslint

--- src/components/Button/Styled.ts
+++ src/components/Button/Styled.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 import { Button } from 'antd'
 
-export const StyledButton = styled(Button)``
 export const StyledResisterButton = styled(Button)`
   position: absolute;
   top: 55px;
